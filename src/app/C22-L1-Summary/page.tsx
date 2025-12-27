0fd0b5ca914d40eed8a5058c7bf6ea6117d7a8f5
mohammad asad <asadkhan41376@gmail.com>
2025-12-27 11:41:49 +0530

    bug fix C22-L1-Summary

--- src/app/C22-L1-Summary/page.tsx
+++ src/app/C22-L1-Summary/page.tsx
@@ -8,7 +8,7 @@ const Page = () => {
     <h2 className='min-h-[80px] text-center text-3xl font-bold text-black'>Video to revise</h2>
 
   <div className='rounded-lg overflow-hidden'>
-  <iframe width="800" height="400" src="https://www.youtube.com/embed/EVwsyzJmTHM?si=N5HNfRuXol3DSTMX" title="YouTube video player"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+  <iframe width="800" height="400" src="https://www.youtube.com/embed/zeu9X88g8DE?si=jJuqvUyUe7_O8e1C" title="YouTube video player"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
   </div>
 </div>
   )
